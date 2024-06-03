5b443f2fab19fe2464b58de4efdca2fece90ecea
ganyanlin <ganyanlin@baidu.com>
2024-06-03 20:51:52 +0800

    feat: BPIT-HCM-72056 大文件分片上传

--- src/pages/editor/monaco-editor/index.tsx
+++ src/pages/editor/monaco-editor/index.tsx
@@ -1,11 +1,15 @@
 import {useState, useRef, useMemo} from 'react'
-import {Button, Input} from 'antd';
+import {Button, Input, Link} from 'antd';
 import style from './style.module.scss';
 
 export const MonacoEditor = () => {
   return (
     <div className={style.monacoEditor}>
-      
+      <Link href="https://aydk.site/editor/enumerations.html">Monaco-editor 学习文档</Link>
+      <Link href="https://github.com/microsoft/monaco-editor">Monaco Editor (github)</Link>
+      <Link href="https://microsoft.github.io/monaco-editor/">Monaco Editor 官网</Link>
+      <Link href="https://microsoft.github.io/monaco-editor/playground.html">Monaco Editor Playground</Link>
+      <Link href="https://microsoft.github.io/monaco-editor/typedoc/index.html">Monaco Editor API</Link>
     </div>
   );
 };
